82089c62fc30087b2525425ec2593d89b225bf59
MuellerPeter <mueller.peter@atz-zwickau.de>
2024-04-17 11:51:21 +0200

    integrate proto in lib

--- build.rs
+++ build.rs
@@ -1,19 +1,33 @@
-
 use std::fs::File;
 use std::io::copy;
+
 use reqwest::Url;
 
+const PROTO_FILE_NAME: &str = "./analogy/analogy.proto";
 fn main() {
-    // url to proto file 
+    // url to proto file
     let url = Url::parse("https://raw.githubusercontent.com/Analogy-LogViewer/Real-Time-Log-Server/main/Analogy.LogServer/Protos/Analogy.proto")
         .expect("Invalid URL");
 
-    // path for storing the proto file 
-    let output_path = std::path::Path::new("./analogy/analogy.proto");
+    // path for storing the proto file
+    let output_path = std::path::Path::new(PROTO_FILE_NAME);
     std::fs::create_dir_all("./analogy").expect("Failed to create path.");
-    
+
     // send request and download file
     let response = reqwest::blocking::get(url).expect("Failed to send request");
     let mut output_file = File::create(output_path).expect("Failed to create file");
-    copy(&mut response.bytes().expect("Failed to get responses bytes").as_ref(), &mut output_file).expect("Failed to save file");
+    copy(
+        &mut response
+            .bytes()
+            .expect("Failed to get responses bytes")
+            .as_ref(),
+        &mut output_file,
+    )
+    .expect("Failed to save file");
+
+    tonic_build::configure()
+        .build_server(false)
+        .out_dir("src/analogy")
+        .compile(&[PROTO_FILE_NAME], &["analogy/"])
+        .expect("Failed to build tonic gRpc client");
 }
