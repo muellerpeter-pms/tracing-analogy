2b5e9c7e55c6feddc7a377d4e34f267c4ffd4f44
MuellerPeter <mueller.peter@atz-zwickau.de>
2024-04-18 04:54:28 +0200

    send test message

---
+++ tests/analogy_viewer.rs
@@ -0,0 +1,29 @@
+//! Test for analogy viewer with self hosted server.
+//!
+//! server is expected to listen as "http://localhost:6000"
+
+use std::time::Duration;
+
+use tokio::time::sleep;
+use tracing::{error, trace};
+use tracing_analogy::AnalogyLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+#[tokio::test]
+async fn test_message() {
+    let layer = AnalogyLayer::new("http://localhost:6000").await.unwrap();
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::new("error,test=TRACE"))
+        .with(layer)
+        .init();
+
+    error!(target: "test", "This is a test message");
+    trace!(
+        target: "test",
+        field = 4,
+        "This is a test message containing fields"
+    );
+
+    sleep(Duration::from_millis(200)).await;
+}
